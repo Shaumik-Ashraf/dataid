7527472cd39128ccf7c2f68c38461174eba3bcda
Shaumik-Ashraf <a.shaumik@gmail.com>
2021-03-01 13:45:25 -0500

    add in source code files

---
+++ dataid.c
@@ -0,0 +1,126 @@
+//dataid.c
+
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include<limits.h>
+
+#include"dataid.h"
+#include"array.h"
+
+
+//static globals
+static struct {
+	size_t length;
+	size_t capacity;
+	unsigned long* array;
+} chain;
+
+static void (*error_f)(const char*) = 0;
+static size_t init_capacity = 10;
+static char immutable = 0;
+
+
+//static functions
+static void default_error(const char* msg) {	
+	fprintf(stderr, "data id error: %s\n", msg);
+	exit(1);
+}
+
+//special assert for dataid library
+//c -> condition; error handler only called if c is true
+//e -> errno or 0
+//s -> error message prefix if errno, or full error message
+static void dassert(int c, int e, const char* s) {
+	static char buffer[128];
+	if(c && e) { 
+		snprintf(buffer, 128, "data id error: %s: %s\n", s, strerror(errsave));
+		error_f(buffer);
+	}
+	else if(c) {
+		snprintf(buffer, 128, "data id error: %s\n", s);
+		error_f(buffer);
+	}
+}
+
+
+static void destroy_chain() {
+	size_t i;
+	
+	for(i=0; i<chain.length; i++) {
+		if( chain.array[i] ) {
+			free( (void*)chain.array[i] );
+		}
+	}
+	free(chain.array);
+}
+
+
+static void resize_chain() {
+	dassert( !immutable, 0, "data id array has reached set capacity" );
+	
+	void* tmp;
+	chain.capacity *= 2;
+	tmp = realloc(chain.array, chain.capacity);
+	dassert( tmp, errno, "realloc" );
+	chain.array = tmp;
+}
+
+
+//library functions
+void dinit(void (*error_handler)(const char*)) {
+	error_f = error_handler ? error_handler : default_error;
+	dassert(0,0,NULL); //make sure static buffer is initialized
+	
+	chain.length = 0;
+	chain.capacity = init_capacity;
+	dassert(init_capacity, 0, "dinit(): initial capacity cannot be 0");
+	chain.array = malloc(init_capacity * sizeof(unsigned long));
+	dassert(chain.array, errno, "dinit()");
+	
+	atexit(destroy_chain);
+}
+
+void dset_capacity(size_t n, char immutable_flag) {
+	immutable = immutable_flag;
+	init_capacity = n;
+}
+
+void dtrim_capacity() {
+	dassert( !immutable, 0, "cannot trim immutable capacity" );
+	void* tmp = realloc(chain.array, chain.length);
+	dassert( tmp, errno, "realloc" );
+	chain.array = tmp;
+}
+
+did_t dset(void* heapmem) {
+	if( chain.length == chain.capacity ) {
+		resize_chain();
+	}
+	chain.array[ chain.length++ ] = heapmem;
+	dassert( chain.length < INT_MAX, 0, "maximum int value has been reached" );
+	return( (did_t)(chain.length - 1) );
+}
+
+did_t dclone(void* x, size_t n) {
+	void* y = malloc(n);
+	dassert(y, errno, "malloc");
+	memcpy(y, x, n);
+	return( dset(y) );
+}
+
+void* dget(did_t d) {
+	dassert( (d>chain.length) || (d<0), 0, "invalid data id" );
+	dassert( chain.array[d], 0, "data was removed" );
+	return( chain.array[d] );
+}
+
+void* dremove(did_t d) {
+	void* ret = dget(d);
+	chain.array[d] = 0UL;
+	return(ret);
+}
+
+void dfree(did_t d) {
+	free( dremove(d) );
+}
